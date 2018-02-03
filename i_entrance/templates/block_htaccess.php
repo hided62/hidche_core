c914fd2aa8e8ec8f2a4dda4ebad01bbf808a312a
hide_d <hided62@gmail.com>
2018-02-04 05:43:46 +0900

    서버 차단용 htaccess에 1분짜리 cache control을 추가

--- i_entrance/templates/block_htaccess.php
+++ i_entrance/templates/block_htaccess.php
@@ -1,3 +1,7 @@
+<IfModule mod_headers.c>
+Header set Cache-Control "max-age=60, must-revalidate"
+</IfModule>
+
 Deny from  all
 <?php if(isset($allow_ip) && $allow_ip != ''): ?>
 SetEnvIf X-Forwarded-For ^<?=str_replace('.', '\\.', $allow_ip)?> env_allow_1
