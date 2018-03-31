9c19e4e92b2c3fdfa42c2cfd57403e716be9ffd1
hide_d <hided62@gmail.com>
2018-04-01 02:59:54 +0900

    validator에 errorStr 함수로 묶어서 보여주는 기능 추가

--- src/sammo/Validator.php
+++ src/sammo/Validator.php
@@ -3,4 +3,11 @@ namespace sammo;
 
 class Validator extends \Valitron\Validator{
     protected static $_lang = 'ko';
+
+    public function errorStr(){
+        $errors = array_values((array)$this->errors());
+        $errors = array_map(function($value){return join(', ', $value);}, $errors);
+        $errors = join(', ', $errors);
+        return $errors;
+    }
 }
