cb5662072855d99fa7f4123e0ec65864dd839354
hide_d <hided62@gmail.com>
2018-04-03 02:59:08 +0900

    부대 창 DB 함수를 변경

--- hwe/c_troop.php
+++ hwe/c_troop.php
@@ -6,53 +6,61 @@ include "func.php";
 // $btn, $name, $troop
 
 //로그인 검사
-CheckLogin();
-$connect = dbConn();
+$session = Session::requireLogin()->loginGame()->setReadOnly();
+$generalID = $session->$generalID;
+if(!$generalID){
+    header('Location:..');
+    die();
+}
 
-$query = "select no,nation,troop from general where owner='{$_SESSION['userID']}'";
-$result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
-$me = MYDB_fetch_array($result);
+$db = DB::db();
+
+$me = $db->queryFirstRow('select no,nation,troop from general where no=%i', $generalID);
 
 $name = trim($name);
 $name = addslashes(SQ2DQ($name));
 if($btn == "부 대 창 설" && $name != "" && $me['troop'] == 0) {
-    $query = "insert into troop (name,nation,no) values ('$name','{$me['nation']}','{$me['no']}')";
-    MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+    $db->insert('troop',[
+        'name'=>$name,
+        'nation'=>$me['nation'],
+        'no'=>$me['no']
+    ]);
+    $troopID = $db->insertId();
 
-    $query = "select troop from troop where no='{$me['no']}'";
-    $result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
-    $troop = MYDB_fetch_array($result);
-
-    $query = "update general set troop='{$troop['troop']}' where no='{$me['no']}'";
-    MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+    $db->update('general', [
+        'troop'=>$troopID
+    ], 'no=%i',$me['no']);
 } elseif($btn == "부 대 변 경" && $name != "") {
-    $query = "update troop set name='$name' where no='{$me['no']}'";
-    MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+    $db->update('troop', [
+        'name'=>$name
+    ], 'no=%i',$me['no']);
 } elseif($btn == "부 대 추 방" && $gen != 0) {
-    $query = "update general set troop='0' where no='$gen'";
-    MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+    $db->update('general', [
+        'troop'=>0
+    ], 'no=%i AND troop=(SELECT troop FROM troop WHERE no = %i)', $gen, $me['no']);
 } elseif($btn == "부 대 가 입" && $troop != 0) {
-    $query = "update general set troop='$troop' where no='{$me['no']}'";
-    MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+    $troop = $db->queryFirstField('SELECT troop FROM troop WHERE no = %i', $troop);
+    if($troop){
+        $db->update('general', [
+            'troop'=>$troop
+        ], 'no=%i', $me['no']);
+    }
 } elseif($btn == "부 대 탈 퇴") {
-    $query = "select no from troop where troop='{$me['troop']}'";
-    $result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
-    $troop = MYDB_fetch_array($result);
-
+    $troopLeader = $db->queryFirstField('SELECT `no` FROM troop WHERE troop=%i', $me['troop']);
+    
     //부대장일 경우
-    if($troop['no'] == $me['no']) {
+    if($troopLeader == $me['no']) {
         // 모두 탈퇴
-        $query = "update general set troop='0' where troop='{$me['troop']}'";
-        MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+        $db->update('general', [
+            'troop'=>0
+        ], 'troop=%i',$me['troop']);
         // 부대 삭제
-        $query = "delete from troop where troop='{$me['troop']}'";
-        MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+        $db->delete('troop', 'troop=%i', $me['troop']);
     } else {
-        $query = "update general set troop='0' where no='{$me['no']}'";
-        MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
+        $db->update('general', [
+            'troop'=>0
+        ], 'no=%i', $me['no']);
     }
 }
 
-echo "<script>location.replace('b_troop.php');</script>";
-
-MYDB_close($connect);
+header('Location:b_troop.php');
