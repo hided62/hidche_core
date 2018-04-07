28aa4c004b7931b017bf4c9d813e38f81d7555f9
hide_d <hided62@gmail.com>
2018-04-07 13:44:41 +0900

    정적 분석을 쉽게 하기 위해 일부분 손댐

--- src/sammo/Validator.php
+++ src/sammo/Validator.php
@@ -1,6 +1,9 @@
 <?php
 namespace sammo;
 
+/**
+ * @method \sammo\Validator rule(string, array<string>|string, null|int|string|array<int|string> $option=null)
+ */
 class Validator extends \Valitron\Validator
 {
     protected static $_lang = 'ko';
