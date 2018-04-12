0b17a289c88fe18facf0e1944d993510d4686f6e
hide_d <hided62@gmail.com>
2018-04-13 02:42:10 +0900

    내정 수치를 tm과 일치

--- hwe/func_process.php
+++ hwe/func_process.php
@@ -324,7 +324,7 @@ function process_3(&$general) {
         if($general['level'] == 12 || $general['level'] == 11 || $general['level'] == 9 || $general['level'] == 7 || $general['level'] == 5) { $score *= 1.05; }
 
         $rd = rand() % 100;
-        $r = CriticalRatioDomestic($general, 0);
+        $r = CriticalRatioDomestic($general, 2);
         // 특기보정 : 발명
         if($general['special'] == 3) { $score *= 1.1; $admin['develcost'] *= 0.8; $r['succ'] += 10; }
 
@@ -520,7 +520,7 @@ function process_5(&$general, $type) {
         if($general['level'] == 4 && $general['no'] == $city['gen1']) { $score *= 1.05; }
 
         $rd = rand() % 100;   // 현재 20%
-        $r = CriticalRatioDomestic($general, 0);
+        $r = CriticalRatioDomestic($general, 1);
         // 특기보정 : 수비, 축성
         if($type == 1 && $general['special'] == 11) { $r['succ'] += 10; $score *= 1.1; $admin['develcost'] *= 0.8; }
         if($type == 2 && $general['special'] == 10) { $r['succ'] += 10; $score *= 1.1; $admin['develcost'] *= 0.8; }
@@ -709,7 +709,7 @@ function process_8(&$general) {
         if($general['level'] == 4 && $general['no'] == $city['gen1']) { $score *= 1.05; }
 
         $rd = rand() % 100;   // 현재 20%
-        $r = CriticalRatioDomestic($general, 0);
+        $r = CriticalRatioDomestic($general, 1);
         // 특기보정 : 통찰
         if($general['special'] == 12) { $r['succ'] += 10; $score *= 1.1; $admin['develcost'] *= 0.8; }
 
