002b7ae5342b2815a066e89d2c55e557a5762a1c
hide_d <hided62@gmail.com>
2018-04-18 23:20:47 +0900

    등용장 버그 수정

--- hwe/func_process_personnel.php
+++ hwe/func_process_personnel.php
@@ -47,16 +47,8 @@ function process_22(&$general) {
         $ded = CharDedication($ded, $general['personal']);
 
         $msg = ScoutMessage::buildScoutMessage($general['no'], $who, $reason);
+        $msg->send();
         
-/*
-        sendScoutMsg([
-            'id' => $general['no'],
-            'nation_id' => Util::array_get($general['nation'], 0)
-        ],[
-            'id' => $you['no'],
-            'nation_id' => Util::array_get($you['nation'], 0)
-        ],$date);
- */
         $general['intel2']++;
         $query = "update general set resturn='SUCCESS',gold=gold-'$cost',intel2='{$general['intel2']}',dedication=dedication+'$ded',experience=experience+'$exp' where no='{$general['no']}'";
         MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
