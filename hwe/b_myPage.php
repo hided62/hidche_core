70b1f23a491dfaec625dc1dc0b93aa090d0af7d5
hide_d <hided62@gmail.com>
2018-05-05 23:25:21 +0900

    빙의 해제용 삭턴 세팅. - 10분 미만은 30분 - 10분 이하는 60분 대기 필요

--- hwe/b_myPage.php
+++ hwe/b_myPage.php
@@ -7,6 +7,7 @@ include "func.php";
 $btn = Util::getReq('btn');
 $mode = Util::getReq('mode', 'int', 2);
 $tnmt = Util::getReq('tnmt', 'int', 1);
+$detachNPC = Util::getReq('detachNPC', 'bool');
 
 extractMissingPostToGlobals();
 
@@ -27,9 +28,10 @@ $connect=$db->get();
 
 increaseRefresh("내정보", 1);
 
-$query = "select myset from general where owner='{$userID}'";
+$query = "select no,npc,mode,tnmt,myset from general where owner='{$userID}'";
 $result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect), "");
 $me = MYDB_fetch_array($result);
+
 
 if ($me['myset'] > 0) {
     $submit = 'submit';
@@ -37,23 +39,37 @@ if ($me['myset'] > 0) {
     $submit = 'hidden';
 }
 
-if ($btn == "설정저장" && $me['myset'] > 0) {
+if (($btn == "설정저장" || $detachNPC) && $me['myset'] > 0) {
     if ($me['myset'] > 1) {
         $submit = 'submit';
     } else {
         $submit = 'hidden';
     }
 
+    $me['myset'] -= 1;
+
     $db->update('general', [
         'myset'=>$db->sqleval('myset-1'),
         'mode'=>$mode,
         'tnmt'=>$tnmt
     ], 'owner=%i', $userID);
-}
 
-$query = "select no,mode,tnmt,myset from general where owner='{$userID}'";
-$result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect), "");
-$me = MYDB_fetch_array($result);
+    if($me['npc'] == 1 && $detachNPC){
+        $turnterm = $db->queryFirstField('SELECT turnterm FROM game LIMIT 1');
+
+        if($turnterm < 10){
+            $targetKillTurn = 30 / $turnterm;
+        }
+        else{
+            $targetKillTurn = 60 / $turnterm;
+        }
+        $db->update('general', [
+            'killturn'=>$targetKillTurn
+        ], 'owner=%i AND npc=1', $userID);
+
+        $me['killturn']=$targetKillTurn;
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -63,13 +79,6 @@ $me = MYDB_fetch_array($result);
 <title><?=UniqueConst::$serverName?>: 내정보</title>
 <link href="../d_shared/common.css" rel="stylesheet">
 <link rel=stylesheet href="css/common.css">
-<script type="text/javascript">
-function go(type) {
-    if(type == 0){ 
-        location.replace('c_vacation.php');
-    }
-}
-</script>
 
 </head>
 
@@ -83,23 +92,25 @@ function go(type) {
             <?php myInfo(); ?>
             <?php myInfo2(); ?>
         </td>
-        <td width=50% valign=top>
+        <td width=50% valign=top style="padding-left:4ch;">
             <form name=form1 action=b_myPage.php method=post>
-                &nbsp;&nbsp;&nbsp;&nbsp;토너먼트 【
+                토너먼트 【
                 <input type=radio name=tnmt value=0 <?=$me['tnmt']==0?"checked":""; ?>>수동참여
                 <input type=radio name=tnmt value=1 <?=$me['tnmt']==1?"checked":""; ?>>자동참여
                 】<br>
-                &nbsp;&nbsp;&nbsp;&nbsp;∞<font color=orange>개막직전 남는자리가 있을경우 랜덤하게 참여합니다.</font><br><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;수비 【
+               ∞<font color=orange>개막직전 남는자리가 있을경우 랜덤하게 참여합니다.</font><br><br>
+                수비 【
                 <input type=radio name=mode  value=2 <?=$me['mode']==2?"checked":""; ?>>◎(훈사80)
                 <input type=radio name=mode  value=1 <?=$me['mode']==1?"checked":""; ?>>○(훈사60)
                 <input type=radio name=mode  value=0 <?=$me['mode']==0?"checked":""; ?>>×
                 】<br><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;<input type=<?=$submit?> name=btn style=background-color:<?=GameConst::$basecolor2?>;color:white;width:160px;height:30px;font-size:13px; value=설정저장><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;∞<font color=orange>설정저장은 이달중 <?=$me['myset']?>회 남았습니다.</font><br><br>
+                <input type=<?=$submit?> name=btn style=background-color:<?=GameConst::$basecolor2?>;color:white;width:160px;height:30px;font-size:13px; value=설정저장><br>
+                ∞<font color=orange>설정저장은 이달중 <?=$me['myset']?>회 남았습니다.</font><br><br>
             </form>
-            &nbsp;&nbsp;&nbsp;&nbsp;휴 가 신 청<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;<input type=button style=background-color:<?=GameConst::$basecolor2?>;color:white;width:160px;height:30px;font-size:13px; value=휴가신청 onclick='go(0)'>
+            휴 가 신 청<br>
+            <a href="c_vacation.php"><button type="button" style=background-color:<?=GameConst::$basecolor2?>;color:white;width:160px;height:30px;font-size:13px;>휴가 신청</button></a><br><br>
+            빙의 해제용 삭턴 조절<br>
+            <a href="b_myPage.php?detachNPC=1"><button type="button" style=background-color:<?=GameConst::$basecolor2?>;color:white;width:160px;height:30px;font-size:13px;>빙의 해체 요청</button></a>
         </td>
     </tr>
     <tr>
