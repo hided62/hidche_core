2fbf8bc2e2338efca3a99ea3a9979d46b111dba8
hide_d <hided62@gmail.com>
2018-05-21 05:30:38 +0900

    누락된 GameCustomConst 추가

---
+++ hwe/d_setting/GameCustomConst.orig.php
@@ -0,0 +1,12 @@
+<?php
+namespace sammo;
+
+class GameCustomConst
+{
+    /** @var int 유저 스탯 총합 */
+    public static $defaultStatTotal = _tK_statTotal_;
+    public static $defaultStatMin = _tK_statMin_;
+    public static $defaultStatMax = _tK_statMax_;
+    public static $defaultStatNPCMax = _tK_statNPCMax_;
+    public static $chiefStatMin = _tK_statChiefMin_;
+}
