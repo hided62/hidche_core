6edaf79df1feaab90b140873e91257239e20518c
hide_d <hided62@gmail.com>
2018-07-18 22:26:15 +0900

    출력 병종 에러

--- hwe/process_war.php
+++ hwe/process_war.php
@@ -528,7 +528,7 @@ function processWar($general, $city) {
 
             $josaYi = JosaUtil::pick($opposeCrewType->name, '이');
             $josaWa = JosaUtil::pick($opposeCrewType->name, '와');
-            $alllog[] = "<C>●</>{$month}월:<Y>{$general['name']}</>의 ".$opposeCrewType->name."{$josaWa} <Y>{$oppose['name']}</>의 ".$opposeCrewType->name."{$josaYi} 대결합니다.";
+            $alllog[] = "<C>●</>{$month}월:<Y>{$general['name']}</>의 ".$generalCrewType->name."{$josaWa} <Y>{$oppose['name']}</>의 ".$opposeCrewType->name."{$josaYi} 대결합니다.";
             $josaUl = JosaUtil::pick($opposeCrewType->name, '을');
             $josaRo = JosaUtil::pick($opposeCrewType->name, '로');
             $log[] = "<C>●</>".$opposeCrewType->name."{$josaRo} <Y>{$oppose['name']}</>의 ".$opposeCrewType->name."{$josaUl} <M>공격</>합니다.";
