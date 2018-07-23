9c666bf7a9a7ec4e05423911e708d8816b453709
hide_d <hided62@gmail.com>
2018-07-23 22:37:30 +0900

    FileDB 클래스 추가

---
+++ src/sammo/FileDB.php
@@ -0,0 +1,19 @@
+<?php
+namespace sammo;
+//SQLite + catfan\Medoo
+
+use \Medoo\Medoo;
+
+class FileDB{
+    public static function DB(string $path, ?string $schemaPath = null) : Medoo{
+        //Note: reference count 적용. MySQL의 것과 다르게 동작함.
+        $db = new Medoo([
+            'database_type' => 'sqlite',
+	        'database_file' => $path
+        ]);
+        if($schemaPath){
+            $db->query(\file_get_contents($schemaPath));
+        }
+        return $db;
+    }
+}
