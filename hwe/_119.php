9d1d51d0d73c1408a3ee26c74d740ce79be50acd
hide_d <hided62@gmail.com>
2018-09-14 00:58:55 +0900

    119 코드에서 $connect 제거

--- hwe/_119.php
+++ hwe/_119.php
@@ -28,13 +28,10 @@ if($session->userGrade < 4) {
 
 $db = DB::db();
 $gameStor = KVStorage::getStorage($db, 'game_env');
-$connect=$db->get();
 
 [$turntime, $tnmt_time] = $gameStor->getValuesAsArray(['turntime','tnmt_time']);
 
-$query = "select plock from plock";
-$result = MYDB_query($query, $connect) or Error(__LINE__.MYDB_error($connect),"");
-$plock = MYDB_fetch_array($result);
+$plock = $db->queryFirstField('SELECT plock FROM plock');
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,7 +48,7 @@ $plock = MYDB_fetch_array($result);
     시간조정 : <input type=text size=3 name=minute><input type=submit name=btn value='분당김'><input type=submit name=btn value='분지연'> 최종갱신 : <?=$turntime?><br>
     시간조정 : <input type=text size=3 name=minute2><input type=submit name=btn value='토너분당김'><input type=submit name=btn value='토너분지연'> 토너먼트 : <?=$tnmt_time?><br>
     봉급지급 : <input type=submit name=btn value='금지급'><input type=submit name=btn value='쌀지급'><br>
-    락 풀 기 : <input type=submit name=btn value='락걸기'><input type=submit name=btn value='락풀기'> 현재 : <?=$plock['plock']>0?"동결중":"가동중"?><br>
+    락 풀 기 : <input type=submit name=btn value='락걸기'><input type=submit name=btn value='락풀기'> 현재 : <?=$plock>0?"동결중":"가동중"?><br>
     </form>
 </body>
 </html>
