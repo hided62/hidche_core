c2f0cbb10a8306c54d0465aea02adc6b661e7dce
hide_d <hided62@gmail.com>
2019-01-16 02:18:42 +0900

    버튼 이동 코드 정리

--- hwe/js/main.js
+++ hwe/js/main.js
@@ -27,7 +27,6 @@ function refreshing(obj, arg1, arg2) {
 //    if(term <= 0) {
     switch(arg1) {
         case 0: location.reload(); break;
-        case 1: go(arg2); break;
         case 2: turn(arg2); break;
         case 3: arg2.submit(); break;
         case 4:
@@ -42,27 +41,6 @@ function refreshing(obj, arg1, arg2) {
 function moveProcessing(commandtype, turn){
     console.log(commandtype, turn);
     $.redirect("processing.php",{ commandtype: commandtype, turn: turn}, 'post'); 
-}
-
-function go(type) {
-    if(type == 1) location.replace('b_nationboard.php');
-    else if(type == 2) location.replace('b_troop.php');
-    else if(type == 3) location.replace('b_nationrule.php');
-    else if(type == 4) location.replace('b_chiefboard.php');
-    else if(type == 5) location.replace('b_chiefcenter.php');
-    else if(type == 6) window.open('b_genList.php');
-    else if(type == 7) location.replace('b_myKingdomInfo.php');
-    else if(type == 8) location.replace('b_myCityInfo.php');
-    else if(type == 9) location.replace('b_myGenInfo.php');
-    else if(type == 10) location.replace('b_myBossInfo.php');
-    else if(type == 11) location.replace('b_currentCity.php');
-    else if(type == 12) location.replace('b_myPage.php');
-    else if(type == 13) location.replace('b_dipcenter.php');
-    else if(type == 14) location.replace('b_diplomacy.php');
-    else if(type == 15) window.open('b_tournament.php');
-    else if(type == 16) window.open('b_betting.php');
-    else if(type == 17) window.open('b_auction.php');
-    else if(type == 18) window.open('b_battleCenter.php');
 }
 
 function turn(type) {
