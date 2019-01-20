7c46d2e27f159f0c0a0c8e9fa5500a7a5496bf80
hide_d <hided62@gmail.com>
2019-01-21 01:45:32 +0900

    게시판 글, 댓글 작성 초안

--- js/common.js
+++ js/common.js
@@ -182,3 +182,16 @@ function getIconPath(imgsvr,  picture){
         return pathConfig.root+'/d_pic/'+picture;
     }
 }
+
+function errUnknown(){
+    alert('작업을 실패했습니다.');
+}
+
+function quickReject(errMsg){
+    if(errMsg === undefined){
+        errMsg = '작업을 실패했습니다.';
+    }
+    var deferred = $.Deferred();
+    deferred.reject(errMsg);
+    return deferred.promise();
+}
