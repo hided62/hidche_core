0e2cab64093c1927c2322494d5afcb9137cb72db
hide_d <hided62@gmail.com>
2019-03-26 15:44:04 +0900

    명전 재 갱신 코드 추가

---
+++ hwe/_admin_force_rehall.php
@@ -0,0 +1,26 @@
+<?php
+namespace sammo;
+
+include "lib.php";
+include "func.php";
+//로그인 검사
+$session = Session::requireGameLogin()->setReadOnly();
+
+if($session->userGrade < 5) {
+    header('location:_admin4.php');
+    die();
+}
+
+$db = DB::db();
+$gameStor = KVStorage::getStorage($db, 'game_env');
+
+if(!$gameStor->isunited){
+    die('아직 천통하지 않았습니다');
+}
+
+foreach($db->queryFirstColumn(
+    'SELECT no FROM general WHERE npc != 0 and age >= %i',
+    GameConst::$retirementYear
+) as $generalNo){
+    CheckHall($generalNo);
+}
