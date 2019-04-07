f1dd328c38c8247b7e4e2bc405ca56a612f45a93
hide_d <hided62@gmail.com>
2019-04-07 19:51:19 +0900

    [864b94f62a]반계 상향

--- hwe/sammo/ActionSpecialWar/che_반계.php
+++ hwe/sammo/ActionSpecialWar/che_반계.php
@@ -9,7 +9,7 @@ class che_반계 implements iAction{
 
     static $id = 45;
     static $name = '반계';
-    static $info = '[전투] 상대의 계략을 30% 확률로 되돌림, 반목 성공시 대미지 추가(+60% → +100%)';
+    static $info = '[전투] 상대의 계략 성공 확률 -10%p, 상대의 계략을 40% 확률로 되돌림, 반목 성공시 대미지 추가(+60% → +100%)';
 
     static $selectWeightType = SpecialityConst::WEIGHT_NORM;
     static $selectWeight = 1;
