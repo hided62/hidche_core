3d2dbe9199cca767e7472916d5a779b8f19aa53c
hide_d <hided62@gmail.com>
2020-05-04 02:04:34 +0900

    루드라사움

--- hwe/sammo/ResetHelper.php
+++ hwe/sammo/ResetHelper.php
@@ -345,6 +345,7 @@ class ResetHelper{
 
         $prefix = DB::prefix();
         AppConf::getList()[$prefix]->closeServer();
+        opcache_reset();
 
         return [
             'result'=>true
