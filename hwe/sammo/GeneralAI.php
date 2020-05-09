d3a9a7b54c361a6ce1c65b003ed01ec06bd94562
hide_d <hided62@gmail.com>
2020-05-09 17:00:36 +0900

    버그 수정

--- hwe/sammo/GeneralAI.php
+++ hwe/sammo/GeneralAI.php
@@ -1064,7 +1064,7 @@ class GeneralAI
                 }
 
                 $crewtype = $targetUserGeneral->getCrewTypeObj();
-                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetUserGeneral->getLeadership(false)) * 2 * 4 * 1.1;
+                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetUserGeneral->getLeadership(false)) * 100 * 2 * 2 * 1.1;
                 if ($this->env['year'] > $this->env['startyear'] + 3) {
                     $reqMoney = max($reqMoney, $reqHumanMinRes);
                 }
@@ -1164,7 +1164,7 @@ class GeneralAI
                 }
 
                 $crewtype = $targetUserGeneral->getCrewTypeObj();
-                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetUserGeneral->getLeadership(false)) * 2 * 4 * 1.1;
+                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetUserGeneral->getLeadership(false)) * 100 * 2 * 2 * 1.1;
                 if ($this->env['year'] > $this->env['startyear'] + 3) {
                     $reqMoney = max($reqMoney, $reqHumanMinRes);
                 }
@@ -1248,7 +1248,7 @@ class GeneralAI
                 }
 
                 $crewtype = $targetNPCGeneral->getCrewTypeObj();
-                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetNPCGeneral->getLeadership(false)) * 2 * 4 * 1.1;
+                $reqMoney = $crewtype->costWithTech($this->nation['tech'], $targetNPCGeneral->getLeadership(false)) * 100 * 2 * 2 * 1.1;
                 if ($this->env['year'] > $this->env['startyear'] + 5) {
                     $reqMoney = max($reqMoney, $reqNPCMinWarRes);
                 }
@@ -1335,7 +1335,7 @@ class GeneralAI
                 }
 
                 $crewtype = $targetNPCGeneral->getCrewTypeObj();
-                $reqMoney = $crewtype->costWithTech($nation['tech'], $targetNPCGeneral->getLeadership(false)) * 2 * 4 * 1.1;
+                $reqMoney = $crewtype->costWithTech($nation['tech'], $targetNPCGeneral->getLeadership(false)) * 100 * 2 * 2 * 1.1;
                 if ($this->env['year'] > $this->env['startyear'] + 5) {
                     $reqMoney = max($reqMoney, $reqNPCMinWarRes);
                 }
