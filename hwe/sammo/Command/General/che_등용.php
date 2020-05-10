bda21c421f95fc1acf4063c58afa606e81f28205
hide_d <hided62@gmail.com>
2020-05-11 01:53:15 +0900

    등용장 버그

--- hwe/sammo/Command/General/che_등용.php
+++ hwe/sammo/Command/General/che_등용.php
@@ -172,7 +172,6 @@ class che_등용 extends Command\GeneralCommand{
         $general->setResultTurn(new LastTurn(static::getName(), $this->arg));
         $general->checkStatChange();
         $general->applyDB($db);
-        $this->destGeneralObj->applyDB($db);
 
         return true;
     }
