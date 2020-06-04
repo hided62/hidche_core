b3f8ed2335350d454c7a3fe7729a0135b2d0c685
hide_d <hided62@gmail.com>
2020-06-05 01:14:49 +0900

    지도 기반 선택 버그 수정

--- hwe/js/defaultSelectNationByMap.js
+++ hwe/js/defaultSelectNationByMap.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function() {
     var $target = $("#destNationID");
     reloadWorldMap({
-        isDetailMap:false,
-        clickableAll:true,
-        neutralView:true,
-        useCachedMap:true,
-        selectCallback:function(city){
+        isDetailMap: false,
+        clickableAll: true,
+        neutralView: true,
+        useCachedMap: true,
+        selectCallback: function(city) {
             var currVal = $target.val();
-            $target.val(city.nationId);
-            if($target.val() === null){
-                $target.val(currVal);
+            $target.val(city.nationId).trigger("change");
+            if ($target.val() === null) {
+                $target.val(currVal).trigger("change");
             }
             return false;
         }
