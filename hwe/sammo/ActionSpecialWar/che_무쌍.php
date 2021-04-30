304099667177cf96a72b373ce46f7d84db835e84
hide_d <hided62@gmail.com>
2021-04-30 16:35:34 +0900

    무쌍 누락분 수정

--- hwe/sammo/ActionSpecialWar/che_무쌍.php
+++ hwe/sammo/ActionSpecialWar/che_무쌍.php
@@ -10,7 +10,7 @@ class che_무쌍 extends \sammo\BaseSpecial{
 
     protected $id = 61;
     protected $name = '무쌍';
-    protected $info = '[전투] 대미지 +10%, 공격 시 필살 확률 +10%p, <br>승리 수만큼 대미지 0.20%씩 추가 상승(최대40%)<br>승리 수만큼 피해 0.05%씩 감소(최대50%)';
+    protected $info = '[전투] 대미지 +10%, 피해 -5%, 공격 시 필살 확률 +10%p, <br>승리 수만큼 대미지 0.20%씩 추가 상승(최대40%)<br>승리 수만큼 피해 0.05%씩 감소(최대30%)';
 
     static $selectWeightType = SpecialityHelper::WEIGHT_NORM;
     static $selectWeight = 1;
@@ -27,10 +27,10 @@ class che_무쌍 extends \sammo\BaseSpecial{
 
     public function getWarPowerMultiplier(WarUnit $unit):array{
         $attackMultiplier = 1.1;
-        $defenceMultiplier = 1;
+        $defenceMultiplier = 0.95;
         $killnum = $unit->getGeneral()->getRankVar('killnum');
         $attackMultiplier += Util::valueFit($killnum * 0.01 * 0.2, null, 0.4);
-        $defenceMultiplier -= Util::valueFit($killnum * 0.01 * 0.05, null, 0.5);
+        $defenceMultiplier -= Util::valueFit($killnum * 0.01 * 0.05, null, 0.3);
         return [$attackMultiplier, $defenceMultiplier];
     }
 }
