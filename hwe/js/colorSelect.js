e2b09caff33ef0415742d2f25303c1ef756ecbbf
hide_d <hided62@gmail.com>
2021-08-20 03:24:14 +0900

    build: processing parts

--- hwe/js/colorSelect.js
+++ hwe/js/colorSelect.js
@@ -1,14 +1,2 @@
-jQuery(function($){
-    function changeNationColorPlate(){
-        var $this = $('#colorType');
-        var $option = $this.find('option:selected');
-        $this.css({
-            'background-color':$option.css('background-color'),
-            'color':$option.css('color')
-        });
-    }
-    $('#colorType').change(function(){
-        changeNationColorPlate();
-    });
-    changeNationColorPlate();
-});
+(()=>{"use strict";var e,r={8730:(e,r,o)=>{o(9826);var n=o(9755);o.n(n)()((function(e){function r(){var r=e("#colorType"),o=r.find("option:selected");r.css({"background-color":o.css("background-color"),color:o.css("color")})}e("#colorType").change((function(){r()})),r()}))}},o={};function n(e){var t=o[e];if(void 0!==t)return t.exports;var i=o[e]={id:e,loaded:!1,exports:{}};return r[e].call(i.exports,i,i.exports,n),i.loaded=!0,i.exports}n.m=r,e=[],n.O=(r,o,t,i)=>{if(!o){var l=1/0;for(u=0;u<e.length;u++){for(var[o,t,i]=e[u],c=!0,a=0;a<o.length;a++)(!1&i||l>=i)&&Object.keys(n.O).every((e=>n.O[e](o[a])))?o.splice(a--,1):(c=!1,i<l&&(l=i));if(c){e.splice(u--,1);var d=t();void 0!==d&&(r=d)}}return r}i=i||0;for(var u=e.length;u>0&&e[u-1][2]>i;u--)e[u]=e[u-1];e[u]=[o,t,i]},n.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return n.d(r,{a:r}),r},n.d=(e,r)=>{for(var o in r)n.o(r,o)&&!n.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:r[o]})},n.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(e){if("object"==typeof window)return window}}(),n.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.nmd=e=>(e.paths=[],e.children||(e.children=[]),e),n.j=331,(()=>{var e={331:0};n.O.j=r=>0===e[r];var r=(r,o)=>{var t,i,[l,c,a]=o,d=0;for(t in c)n.o(c,t)&&(n.m[t]=c[t]);if(a)var u=a(n);for(r&&r(o);d<l.length;d++)i=l[d],n.o(e,i)&&e[i]&&e[i][0](),e[l[d]]=0;return n.O(u)},o=self.webpackChunkhidche_lib=self.webpackChunkhidche_lib||[];o.forEach(r.bind(null,0)),o.push=r.bind(null,o.push.bind(o))})();var t=n.O(void 0,[216],(()=>n(8730)));t=n.O(t)})();
+//# sourceMappingURL=colorSelect.js.map
