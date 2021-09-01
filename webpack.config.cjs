acd77b2c0acf6164bc840bd00b0e64b04e1168ed
hide_d <hided62@gmail.com>
2021-09-02 01:49:30 +0900

    web_pack_config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,8 +5,151 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const { resolve } = require('path');
 
 module.exports = (env, argv) => {
-    const target = env.target??'hwe';
+    const target = env.target ?? 'hwe';
     const mode = argv.mode ?? 'production';
+    const ingame_vue = {
+        name: 'ingame_vue',
+        resolve: {
+            extensions: [".js", ".ts", ".jsx", ".tsx", ".vue"],
+            alias: {
+                vue: "@vue/runtime-dom"
+            }
+        },
+        mode,
+        entry: {
+            //v_test: resolve(__dirname, `${target}/ts/v_test.ts`)
+        },
+        output: {
+            filename: '[name].js',
+            path: resolve(__dirname, `${target}/dist_js`)
+        },
+        devtool: 'source-map',
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors_vue',
+                        chunks: 'all',
+                    },
+                },
+            }
+        },
+        module: {
+            rules: [
+                //FROM `vue inspect` and some tweaks
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /(node_modules)/,
+                    use: [
+                        'babel-loader',
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true,
+                                appendTsSuffixTo: [
+                                    '\\.vue$'
+                                ],
+                                happyPackMode: false
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    exclude: /(node_modules)/,
+                    options: {
+                        /*transformAssetUrls: {
+                            png: [],
+                        },*/
+                        hotReload: false,
+                    }
+                },
+                {
+                    test: /\.vue\.(s?[ac]ss)$/,
+                    use: ['vue-style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /(?<!\.vue)\.(s?[ac]ss)$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(png|jpe?g|gif|webp)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: '../dist_misc/[name].[contenthash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(svg)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '../dist_misc/[name].[contenthash:8].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: '../dist_misc/[name].[contenthash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(woff2?|eot|ttf|otf)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: '../dist_misc/[name].[contenthash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+            ]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '../dist_css/[name].css'
+            }),
+            //new BundleAnalyzerPlugin()
+        ],
+        cache: {
+            type: 'filesystem',
+            allowCollectingMemory: true,
+        },
+    };
     const ingame = {
         name: 'ingame',
         resolve: {
@@ -77,29 +220,13 @@ module.exports = (env, argv) => {
                         ]
                     }
                 }, 'ts-loader']
-            }, {
-                test: /\.vue$/i,
-                exclude: /(node_modules)/,
-                use: [
-                    'vue-loader'
-                ]
-            }, {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                ],
-            }, {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
+            },
+            {
+                test: /.(s?[ac]ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }]
         },
         plugins: [
-            new VueLoaderPlugin(),
             new MiniCssExtractPlugin({
                 filename: '../dist_css/[name].css'
             }),
@@ -162,30 +289,12 @@ module.exports = (env, argv) => {
                         ]
                     }
                 }
-            },
-            {
-                test: /\.vue$/i,
-                exclude: /(node_modules)/,
-                use: [
-                    { loader: 'vue-loader' }
-                ]
             }, {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                ],
-            }, {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
+                test: /.(s?[ac]ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }]
         },
         plugins: [
-            new VueLoaderPlugin(),
             new MiniCssExtractPlugin({
                 filename: '../dist_css/[name].css'
             }),
@@ -197,10 +306,10 @@ module.exports = (env, argv) => {
         },
     };
 
-    if(target == 'hwe'){
-        return [ingame, gateway];
+    if (target == 'hwe') {
+        return [ingame_vue, ingame, gateway];
     }
-    else{
-        return [ingame];
+    else {
+        return [ingame_vue, ingame];
     }
 }
