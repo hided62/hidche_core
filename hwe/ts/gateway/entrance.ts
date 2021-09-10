fecb3f1f7b4232fdaa61acadbf01177754e05207
hide_d <hided62@gmail.com>
2021-09-10 21:38:32 +0900

    fix: gateway tooltip

--- hwe/ts/gateway/entrance.ts
+++ hwe/ts/gateway/entrance.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import $ from 'jquery';
+import Popper from 'popper.js';
+exportWindow(Popper, 'Popper');//XXX: 왜 popper를 이렇게 불러야 하는가?
+import 'bootstrap';
 import { initTooltip } from '../common_legacy';
 import { TemplateEngine } from "../util/TemplateEngine";
 import { InvalidResponse } from '../defs';
 import { getDateTimeNow } from '../util/getDateTimeNow';
 import { setAxiosXMLHttpRequest } from '../util/setAxiosXMLHttpRequest';
 import { loadPlugin as loadAdminPlugin } from './admin_server';
+import { exportWindow } from '../util/exportWindow';
 
 declare const isAdmin: boolean;
 
