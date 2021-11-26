03a62dc23fba78981144853468df2fb18ed156d8
hide_d <hided62@gmail.com>
2021-11-27 02:10:08 +0900

    fix(in-progress): viewport width를 360px 폰에서 맞게 수정

--- hwe/index.php
+++ hwe/index.php
@@ -100,7 +100,7 @@ $auctionCount = $db->queryFirstField('SELECT count(`no`) FROM auction');
     <title><?= $serverName ?>: 메인</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.7, minimum-scale=0.7" />
     <?= WebUtil::printJS('../d_shared/common_path.js') ?>
     <?= WebUtil::printJS('dist_js/vendors.js') ?>
     <?= WebUtil::printJS('d_shared/base_map.js') ?>
