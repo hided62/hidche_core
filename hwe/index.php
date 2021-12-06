d86d96acc1f0a5caf79254f4ce965938acffe531
hide_d <hided62@gmail.com>
2021-12-07 01:19:38 +0900

    misc: 초기 스마트폰 크기 관련 수정

--- hwe/index.php
+++ hwe/index.php
@@ -115,7 +115,7 @@ if (!$otherTextInfo) {
     <title><?= $serverName ?>: 메인</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=500" />
+    <meta name="viewport" content="width=500, initial-scale=1" />
     <?= WebUtil::printJS('../d_shared/common_path.js') ?>
     <?= WebUtil::printJS('dist_js/vendors.js') ?>
     <?= WebUtil::printJS('d_shared/base_map.js') ?>
