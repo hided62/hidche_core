311d5c42a8e7c96c25890a79ed0d7c9d2e4061ff
hide_d <hided62@gmail.com>
2021-12-14 02:05:59 +0900

    fix: 메인페이지, 임관권유 페이지에 적용

--- hwe/ts/v_join.ts
+++ hwe/ts/v_join.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import PageJoin from '@/PageJoin.vue';
 import BootstrapVue3 from 'bootstrap-vue-3'
 import "@scss/common/bootstrap5.scss";
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import "@scss/editor_component.scss";
 
 createApp(PageJoin).use(BootstrapVue3).mount('#app')
