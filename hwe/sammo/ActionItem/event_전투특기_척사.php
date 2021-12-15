7631d99ce6aa435e0ebdd1aceaf41f19fdc8c1ec
hide_d <hided62@gmail.com>
2021-12-16 03:12:37 +0900

    game: 척사 강화. 15%p -> 20%p

--- hwe/sammo/ActionItem/event_전투특기_척사.php
+++ hwe/sammo/ActionItem/event_전투특기_척사.php
@@ -10,7 +10,7 @@ class event_전투특기_척사 extends \sammo\BaseItem{
     protected $id = 75;
     protected $rawName = '비급';
     protected $name = '비급(척사)';
-    protected $info = '[전투] 지역·도시 병종 상대로 대미지 +10%, 아군 피해 -10%';
+    protected $info = '[전투] 지역·도시 병종 상대로 대미지 +20%, 아군 피해 -20%';
     protected $cost = 100;
     protected $buyable = true;
     protected $consumable = false;
@@ -19,7 +19,7 @@ class event_전투특기_척사 extends \sammo\BaseItem{
     public function getWarPowerMultiplier(WarUnit $unit):array{
         $opposeCrewType = $unit->getOppose()->getCrewType();
         if($opposeCrewType->reqCities || $opposeCrewType->reqRegions){
-            return [1.1, 0.9];
+            return [1.2, 0.8];
         }
         return [1, 1];
     }
--- hwe/sammo/ActionSpecialDomestic/che_event_척사.php
+++ hwe/sammo/ActionSpecialDomestic/che_event_척사.php
@@ -9,7 +9,7 @@ class che_event_척사 extends \sammo\BaseSpecial{
 
     protected $id = 75;
     protected $name = '척사';
-    protected $info = '[전투] 지역·도시 병종 상대로 대미지 +15%, 아군 피해 -15%';
+    protected $info = '[전투] 지역·도시 병종 상대로 대미지 +20%, 아군 피해 -20%';
 
     static $selectWeightType = SpecialityHelper::WEIGHT_NORM;
     static $selectWeight = 1;
@@ -22,7 +22,7 @@ class che_event_척사 extends \sammo\BaseSpecial{
     public function getWarPowerMultiplier(WarUnit $unit):array{
         $opposeCrewType = $unit->getOppose()->getCrewType();
         if($opposeCrewType->reqCities || $opposeCrewType->reqRegions){
-            return [1.15, 0.85];
+            return [1.2, 0.8];
         }
         return [1, 1];
     }
