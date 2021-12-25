356250ba8cd42cefa2af649ffa70f21259596cf1
hide_d <hided62@gmail.com>
2021-12-25 22:55:16 +0900

    fix: 사령부에 500px 전환 코드 추가

--- hwe/ts/v_chiefCenter.ts
+++ hwe/ts/v_chiefCenter.ts
@@ -4,11 +4,14 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import { setAxiosXMLHttpRequest } from '@util/setAxiosXMLHttpRequest';
 import Multiselect from 'vue-multiselect';
+import { auto500px } from './util/auto500px';
 
 declare const maxChiefTurn: number;
 
 
 setAxiosXMLHttpRequest();
+auto500px();
+
 createApp(PageChiefCenter, {
     maxChiefTurn,
 
