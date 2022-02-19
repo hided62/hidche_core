ac8edcee32210733e2a71ed52c53713e81cbfe2d
hide_d <hided62@gmail.com>
2022-02-20 03:09:59 +0900

    game: 회피 유니크 조절

--- hwe/sammo/ActionItem/che_필살_둔갑천서.php
+++ hwe/sammo/ActionItem/che_필살_둔갑천서.php
@@ -3,20 +3,18 @@ namespace sammo\ActionItem;
 use \sammo\iAction;
 use \sammo\General;
 
-class che_회피_둔갑천서 extends \sammo\BaseItem{
+class che_필살_둔갑천서 extends \sammo\BaseItem{
 
     protected $rawName = '둔갑천서';
-    protected $name = '둔갑천서(회피)';
-    protected $info = '[전투] 회피 확률 +20%p';
+    protected $name = '둔갑천서(필살)';
+    protected $info = '[전투] 필살 확률 +20%p';
     protected $cost = 200;
     protected $consumable = false;
 
     public function onCalcStat(General $general, string $statName, $value, $aux=null){
-        if($statName === 'warAvoidRatio'){
+        if($statName === 'warCriticalRatio'){
             return $value + 0.2;
         }
         return $value;
     }
 }
-
-//NOTE: 구버전
--- hwe/sammo/ActionItem/che_회피_태평요술.php
+++ hwe/sammo/ActionItem/che_회피_태평요술.php
@@ -7,13 +7,13 @@ class che_회피_태평요술 extends \sammo\BaseItem{
 
     protected $rawName = '태평요술';
     protected $name = '태평요술(회피)';
-    protected $info = '[전투] 회피 확률 +20%p';
+    protected $info = '[전투] 회피 확률 +15%p';
     protected $cost = 200;
     protected $consumable = false;
 
     public function onCalcStat(General $general, string $statName, $value, $aux=null){
         if($statName === 'warAvoidRatio'){
-            return $value + 0.2;
+            return $value + 0.15;
         }
         return $value;
     }
