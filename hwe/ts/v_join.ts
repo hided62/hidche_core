54e4d74d0a7c7de6b48228b02b8adc4bc0e85f4e
Hide_D <hided62@gmail.com>
2022-04-08 19:18:51 +0900

    fix: 장수 생성 페이지 너비 수정

--- hwe/ts/v_join.ts
+++ hwe/ts/v_join.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import PageJoin from '@/PageJoin.vue';
 import BootstrapVue3 from 'bootstrap-vue-3'
+import { auto500px } from './util/auto500px';
+
+auto500px();
 
 createApp(PageJoin).use(BootstrapVue3).mount('#app')
