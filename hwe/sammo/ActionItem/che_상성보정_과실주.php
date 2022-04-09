082f847b1fe873edc550b16381e64e05d6f94861
Hide_D <hided62@gmail.com>
2022-04-10 01:17:41 +0900

    game: 과실주(상성)를 대등한 병종에도 발동

--- hwe/sammo/ActionItem/che_상성보정_과실주.php
+++ hwe/sammo/ActionItem/che_상성보정_과실주.php
@@ -10,7 +10,7 @@ class che_상성보정_과실주 extends \sammo\BaseItem{
 
     protected $rawName = '과실주';
     protected $name = '과실주(상성)';
-    protected $info = '[전투] 유리한 병종 전투시 공격력 +10%, 피해 -10%';
+    protected $info = '[전투] 대등/유리한 병종 전투시 공격력 +10%, 피해 -10%';
     protected $cost = 200;
     protected $consumable = false;
 
@@ -18,7 +18,7 @@ class che_상성보정_과실주 extends \sammo\BaseItem{
     {
         $oppose = $unit->getOppose();
         $attackCoef = $unit->getCrewType()->getAttackCoef($oppose->getCrewType());
-        if($attackCoef <= 1){
+        if($attackCoef < 1){
             return null;
         }
         return new WarUnitTriggerCaller(
