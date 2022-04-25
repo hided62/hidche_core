3ac9e7c5993360de8fa27d7560e893e379f64abd
Hide_D <hided62@gmail.com>
2022-04-26 00:39:36 +0900

    misc: 궁병 수비선제사격에서 선제 공격 로그를 단순하게 처리 - 상대방의 필살, 격노도 출력하지 않음

--- hwe/sammo/WarUnitTrigger/che_궁병선제사격.php
+++ hwe/sammo/WarUnitTrigger/che_궁병선제사격.php
@@ -39,7 +39,7 @@ class che_궁병선제사격 extends BaseWarUnitTrigger
                 $oppose->multiplyWarPowerMultiply(0);
                 $self->multiplyWarPowerMultiply(0.5);
                 $self->activateSkill('회피불가', '필살불가', '계략불가');
-                $oppose->activateSkill('회피불가', '계략불가');
+                $oppose->activateSkill('회피불가', '필살불가', '격노불가', '계략불가');
             }
         } else {
             $oppose->multiplyWarPowerMultiply(0.5);
