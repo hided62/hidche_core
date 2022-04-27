72e1d5e03b32dbdd03a0349def208c9c1def8a71
Hide_D <hided62@gmail.com>
2022-04-28 00:54:34 +0900

    fix: 실제 코드 변경에 따라 의술을 1/3가 아니라 30%로 정정

--- hwe/sammo/ActionItem/che_의술_정력견혈산.php
+++ hwe/sammo/ActionItem/che_의술_정력견혈산.php
@@ -13,7 +13,7 @@ class che_의술_정력견혈산 extends \sammo\BaseItem{
 
     protected $rawName = '정력견혈산';
     protected $name = '정력견혈산(의술)';
-    protected $info = '[군사] 매 턴마다 자신(100%)과 소속 도시 장수(적 포함 50%) 부상 회복<br>[전투] 페이즈마다 40% 확률로 치료 발동(아군 피해 1/3 감소, 부상 회복)';
+    protected $info = '[군사] 매 턴마다 자신(100%)과 소속 도시 장수(적 포함 50%) 부상 회복<br>[전투] 페이즈마다 40% 확률로 치료 발동(아군 피해 30% 감소, 부상 회복)';
     protected $cost = 200;
     protected $consumable = false;
 
