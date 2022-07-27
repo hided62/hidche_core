a9fc69e70fdbcab027eeef78192defd65fd42d16
Hide_D <hided62@gmail.com>
2022-07-27 22:52:02 +0900

    game: 벽력거 비용 약 상승, 목우 비용 하향

--- hwe/sammo/GameUnitConstBase.php
+++ hwe/sammo/GameUnitConstBase.php
@@ -329,7 +329,7 @@ class GameUnitConstBase{
         ],
         [
             1502, self::T_SIEGE, '벽력거',
-            200, 100, 6,  5, 0,  18,  5,
+            200, 100, 6,  5, 0,  20,  5,
             3000, ['업'],   null,     0,
             [self::T_FOOTMAN=>1.25, self::T_ARCHER=>1.25, self::T_CAVALRY=>1.25, self::T_WIZARD=>1.25, self::T_CASTLE=>1.8, 1106=>1.112],
             [self::T_FOOTMAN=>0.833, self::T_ARCHER=>0.833, self::T_CAVALRY=>0.833, self::T_WIZARD=>0.833, 1106=>0.909],
@@ -338,7 +338,7 @@ class GameUnitConstBase{
         ],
         [
             1503, self::T_SIEGE, '목우',
-            50, 200, 5,  0, 0,  18,  5,
+            50, 200, 5,  0, 0,  15,  5,
             3000, ['성도'], null,     0,
             [self::T_FOOTMAN=>0.8, self::T_ARCHER=>0.8, self::T_CAVALRY=>0.8, self::T_WIZARD=>0.8],
             [self::T_FOOTMAN=>1.2, self::T_ARCHER=>1.2, self::T_CAVALRY=>1.2, self::T_WIZARD=>1.2],
