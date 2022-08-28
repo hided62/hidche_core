4bf77a5c5579a41e3d222b6ebab5c4223f2a69e0
Hide_D <hided62@gmail.com>
2022-08-28 14:35:14 +0900

    game: 궁병 공격 시도, 추가페이즈 선제 공격 - 수비 선제와 구조적으로 동일 - 첫번째 상대에게 선제 사격 - 궁병과 궁병의 대결인 경우 항상 맞 선제 사격

--- hwe/sammo/WarUnitTrigger/che_궁병선제사격.php
+++ hwe/sammo/WarUnitTrigger/che_궁병선제사격.php
@@ -28,22 +28,28 @@ class che_궁병선제사격 extends BaseWarUnitTrigger
         if ($self->hasActivatedSkillOnLog('선제')) {
             return true;
         }
+        if ($oppose->hasActivatedSkill('선제')){
+            //맞 선제는 한쪽에서 같이 처리
+            return true;
+        }
 
         $self->activateSkill('특수', '선제');
-        if (!$self->isAttacker()) {
-            $self->addPhase(-1);
-            $oppose->addPhase(-1);
-            if ($oppose->getCrewType()->armType == GameUnitConst::T_ARCHER) {
-                $oppose->multiplyWarPowerMultiply(0.5);
-            } else {
-                $oppose->multiplyWarPowerMultiply(0);
-                $self->multiplyWarPowerMultiply(0.5);
-                $self->activateSkill('회피불가', '필살불가', '계략불가');
-                $oppose->activateSkill('회피불가', '필살불가', '격노불가', '계략불가');
-            }
-        } else {
+        $self->addPhase(-1);
+        $oppose->addPhase(-1);
+        if ($oppose->getCrewType()->armType == GameUnitConst::T_ARCHER) {
+            $oppose->activateSkill('특수', '선제');
+
+            $self->multiplyWarPowerMultiply(0.5);
             $oppose->multiplyWarPowerMultiply(0.5);
+            $oppose->getLogger()->pushGeneralBattleDetailLog('서로 <C>선제 사격</>을 주고 받았다!</>', ActionLogger::PLAIN);
+            $self->getLogger()->pushGeneralBattleDetailLog('서로 <C>선제 사격</>을 주고 받았다!</>', ActionLogger::PLAIN);
+            return true;
         }
+
+        $oppose->multiplyWarPowerMultiply(0);
+        $self->multiplyWarPowerMultiply(0.5);
+        $self->activateSkill('회피불가', '필살불가', '계략불가');
+        $oppose->activateSkill('회피불가', '필살불가', '격노불가', '계략불가');
 
         $oppose->getLogger()->pushGeneralBattleDetailLog('상대에게 <R>선제 사격</>을 받았다!</>', ActionLogger::PLAIN);
         $self->getLogger()->pushGeneralBattleDetailLog('상대에게 <C>선제 사격</>을 했다!</>', ActionLogger::PLAIN);
