b28ca864f90e715d868e5b0fe828044d10385d4c
Hide_D <hided62@gmail.com>
2022-08-28 15:30:35 +0900

    dep: package update

--- hwe/ts/util/entriesWithType.ts
+++ hwe/ts/util/entriesWithType.ts
@@ -2,6 +2,6 @@ type Entries<T> = {
     [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
-export function entriesWithType<T>(value: T): Entries<T>{
+export function entriesWithType<T extends object>(value: T): Entries<T>{
     return Object.entries(value) as unknown as Entries<T>;
 }
