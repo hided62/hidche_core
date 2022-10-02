b2a676cd8b3adf7d4fae5166564d25c8b30c05d8
Hide_D <hided62@gmail.com>
2022-10-02 23:25:00 +0900

    game: 옥란백용구 향상 최대 30% -> 50%

--- hwe/sammo/ActionItem/che_명마_12_옥란백용구.php
+++ hwe/sammo/ActionItem/che_명마_12_옥란백용구.php
@@ -10,7 +10,7 @@ class che_명마_12_옥란백용구 extends \sammo\BaseStatItem{
     public function __construct()
     {
         parent::__construct();
-        $this->info .= "<br>[전투] 남은 병력이 적을수록 회피 확률 증가. 최대 +30%p";
+        $this->info .= "<br>[전투] 남은 병력이 적을수록 회피 확률 증가. 최대 +50%p";
     }
 
     public function onCalcStat(General $general, string $statName, $value, $aux = null)
@@ -20,7 +20,7 @@ class che_명마_12_옥란백용구 extends \sammo\BaseStatItem{
             $leadership = $general->getLeadership(true, true, true, false);
             $crewL = $general->getVar('crew') / 100;
 
-            return $value + \sammo\Util::valueFit((1 - $crewL / $leadership) * 0.3, 0, 0.3);
+            return $value + \sammo\Util::valueFit((1 - $crewL / $leadership) * 0.5, 0, 0.5);
         }
         return $value;
     }
