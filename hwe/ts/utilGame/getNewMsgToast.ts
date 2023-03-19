457aa0e4a52d1bb6c50604910e8cdf4e88336834
Hide_D <hided62@gmail.com>
2023-03-19 15:38:49 +0900

    misc: getNewMsgToast 힌트에 따라 수정

--- hwe/ts/utilGame/getNewMsgToast.ts
+++ hwe/ts/utilGame/getNewMsgToast.ts
@@ -1,17 +1,17 @@
 import { BButton } from "bootstrap-vue-next";
 import type { ToastContent } from "bootstrap-vue-next/dist/components/BToast/plugin";
-import { h } from "vue";
+import { createTextVNode, h } from "vue";
 
 type CallbackType = (type: "goto" | "ignore", e: MouseEvent) => void;
 
-export function getNewMsgToast(title: string, body: string, callback: CallbackType): ToastContent{
+export function getNewMsgToast(title: string, body: string, callback: CallbackType): ToastContent {
   const bodyNode = h(
     "span",
     null,
     [
       body,
-      h(BButton, { variant: "primary", size: "sm", onClick: (e)=>{ callback('goto', e) } }, "보러가기"),
-      h(BButton, { variant: "secondary", size: "sm", onClick: (e)=>{ callback('ignore', e)} }, "이미읽음"),
+      h(BButton, { variant: "primary", size: "sm", onClick: (e) => { callback('goto', e) } }, () => createTextVNode("보러가기")),
+      h(BButton, { variant: "secondary", size: "sm", onClick: (e) => { callback('ignore', e) } }, () => createTextVNode("이미읽음")),
     ]
   );
   return {
