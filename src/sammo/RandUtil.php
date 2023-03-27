64aa5d041f0ca6db0be836100e2dd17c74b45d98
sars <sarsntony@gmail.com>
2023-03-27 09:54:37 -0700

    Revert "긴급 버그 패치, 내특 줄 수 있는게 하나도 없으면 강제로 상재로 주고 턴 넘기는것으로 긴급패치, 히데님이 알아서 제대로 고칠것, (2로 해두면 상재가 아니라 아에 버그가 떠서 그냥 롤백시키고 히데님이 다 고치는걸로 방치하기로 결정)"
    This reverts commit fbccfbb65e65cbadb1a9a3afc9025d362e733a9a.

--- src/sammo/RandUtil.php
+++ src/sammo/RandUtil.php
@@ -104,8 +104,7 @@ class RandUtil
     public function choiceUsingWeight(array $items)
     {
         if(!$items){
-	    return 2; // 내특 줄 수 있는게 하나도 없을시 일단 강제로 상재로 주는걸로 긴급 버그 수정	
-	    //throw new \InvalidArgumentException();
+            throw new \InvalidArgumentException();
         }
         $sum = 0;
         foreach ($items as $value) {
