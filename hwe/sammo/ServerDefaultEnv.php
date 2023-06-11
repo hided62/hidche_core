4efcbaed6aa05c90c95ae3e757d796880bd2728d
Hide_D <hided62@gmail.com>
2023-06-11 21:57:45 +0900

    misc: d_setting에 관련 코드가 추가되어 TODO 제거

--- hwe/sammo/ServerDefaultEnv.php
+++ hwe/sammo/ServerDefaultEnv.php
@@ -5,8 +5,6 @@ namespace sammo;
 /** 서버 내 변수 모음 */
 class ServerDefaultEnv
 {
-  //TODO: 이 값이 .env든 d_setting의 기타 변수이든, 수정 가능해야함
-
   private function __construct()
   {
   }
--- hwe/sammo/ServerEnv.php
+++ hwe/sammo/ServerEnv.php
@@ -2,5 +2,5 @@
 namespace sammo;
 
 class ServerEnv extends ServerDefaultEnv {
-    //TODO: 이 값이 .env든 d_setting의 기타 변수이든, 수정 가능해야함
+    //NOTE: d_setting에 ServerEnv.php가 없을때 대비.
 }
