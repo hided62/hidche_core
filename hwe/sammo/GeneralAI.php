2f00aee947382e91a53d8af1717cf0c167f90a84
포스트모더니즘 <>
2024-01-26 22:17:06 +0900

    아주 약간 효율적으로 수정

--- hwe/sammo/GeneralAI.php
+++ hwe/sammo/GeneralAI.php
@@ -3440,9 +3440,9 @@ class GeneralAI
         $cmd = buildGeneralCommandClass($this->rng->choice($candidate), $this->general, $this->env);
         if (!$cmd->hasFullConditionMet()) {
             $cmd = buildGeneralCommandClass('che_물자조달', $this->general, $this->env);
-        }
-        if (!$cmd->hasFullConditionMet()) {
-            $cmd = buildGeneralCommandClass('che_견문', $this->general, $this->env);
+            if (!$cmd->hasFullConditionMet()) {
+                $cmd = buildGeneralCommandClass('che_견문', $this->general, $this->env);
+            }
         }
 
         return $cmd;
